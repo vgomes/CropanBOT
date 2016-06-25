2927ef9b73842cf01550d21a21ca832d33835d32
Victor Gomes <victor.j.gomes@gmail.com>
2016-06-25 13:32:25 +0200

    Changes for openshift DB

--- config/database.php
+++ config/database.php
@@ -66,6 +66,18 @@ return [
             'engine' => null,
         ],
 
+        'openshift' => array(
+            'driver'    => 'mysql',
+            'host'      => getenv('OPENSHIFT_MYSQL_DB_HOST'),
+            'port'      => getenv('OPENSHIFT_MYSQL_DB_PORT'),
+            'database'  => getenv('OPENSHIFT_APP_NAME'),
+            'username'  => getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
+            'password'  => getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ),
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', 'localhost'),
