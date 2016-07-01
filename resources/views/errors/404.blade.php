58421f3b03f894a0e2239e92595ac672c3b5ab27
Victor Gomes <victor.j.gomes@gmail.com>
2016-07-01 07:11:25 +0200

    Added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+    <div class="container-fluid">
+        <img class="img-thumbnail" src="https://media.giphy.com/media/gZUaHprd5mUEM/giphy.gif" width="500px" />
+        <hr>
+        <h2>404 - Not found</h2>
+    </div>
+@endsection
