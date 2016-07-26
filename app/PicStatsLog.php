a95f6a5f75c0b89fb58317bfed6c6a23c3b34407
Victor Gomes <victor.j.gomes@gmail.com>
2016-07-26 12:56:24 +0200

    Fixed a bug preventing accurate stats

--- app/PicStatsLog.php
+++ app/PicStatsLog.php
@@ -8,4 +8,6 @@ class PicStatsLog extends Model
 {
     protected $table = 'picture_stats_log';
     protected $fillable = ['date', 'sent', 'published', 'images_positive', 'images_negative', 'votes', 'votes_yes', 'votes_no'];
+    protected $primaryKey = 'date';
+    public $incrementing = false;
 }
