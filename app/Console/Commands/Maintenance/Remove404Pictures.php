5ff37f2a611c4868effa6ccab0918394fa482c07
Victor Gomes <victor.j.gomes@gmail.com>
2017-03-22 21:11:41 +0100

    Improvements and new command

---
+++ app/Console/Commands/Maintenance/Remove404Pictures.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Cropan\Console\Commands\Maintenance;
+
+use Cropan\Picture;
+use GuzzleHttp\Client;
+use Illuminate\Console\Command;
+
+class Remove404Pictures extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'maintenance:404';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Removes images no longer available';
+
+    protected $client;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->client = new Client();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $pictures = Picture::all();
+
+        $pictures->each(function (Picture $picture) {
+             $answer = $this->client->get($picture->url);
+
+             if ($answer->getStatusCode() === 404) {
+                 \Log::info("Deleting image $picture->id: $picture->url");
+                 $picture->delete();
+             }
+        });
+
+        return true;
+    }
+}
