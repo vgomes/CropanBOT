6ec3f0ad0a44b37f94af75f68f35121223b8c15e
Victor Gomes <victor.j.gomes@gmail.com>
2017-05-30 20:01:02 +0200

    Removed unused dependency

--- config/app.php
+++ config/app.php
@@ -234,8 +234,7 @@ return [
         'Socialite'    => Laravel\Socialite\Facades\Socialite::class,
         'Form'         => Collective\Html\FormFacade::class,
         'Html'         => Collective\Html\HtmlFacade::class,
-        'Telegram'     => Telegram\Bot\Laravel\Facades\Telegram::class,
-        'Instagram'    => Andreyco\Instagram\Support\Laravel\Facade\Instagram::class
+        'Telegram'     => Telegram\Bot\Laravel\Facades\Telegram::class
     ],
 
 ];
